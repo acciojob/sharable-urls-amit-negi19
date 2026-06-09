d1bf14be2c97aff63ac5b3531d7ef378f723d97d
Amit Negi <amitnegi07ammy@gmail.com>
2026-06-09 22:06:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,15 +5,16 @@ const url=document.getElementById("url");
 button.addEventListener("click",function(e){
 	e.preventDefault();
 
-	let name=document.getElementById("name").value;
-	let year=document.getElementById("year").value;
+	const name=document.getElementById("name").value;
+	const year=document.getElementById("year").value;
 
 	let result="https://localhost:8080/";
 
-	if(name && year){result+= `?name=${name}`;
+	if(name&&year){result+= `?name=${name}&year=${year}`;
 	}				 
 	else if{(name)result+=`?name=${name}`;
-    }else if(year){
+    }
+	else if(year){
 	result+= `?year=${year}`;
 	}
 
