eec285d8bd53e0fb69a8ef28111fe725ffd583a8
Amit Negi <amitnegi07ammy@gmail.com>
2026-06-09 21:46:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 // your code here
+const form=document.getElementById("form");
+const url=document.getElementById("url");
+
+form.addEventListener("submit",function(e){
+	e.preventDefault();
+
+	let name=document.getElementById("name").value;
+	let year=document.getElementById("year").value;
+
+	let query[];
+
+	if(name)query.push(`name=${name}`);
+	if(year)query.push(`year=${year}`);
+
+	url.textContent="https://localhost:8080/";
+	if(query.length>0){
+		url.textContent +="?"+query.join("&");
+	}
+	
+});
